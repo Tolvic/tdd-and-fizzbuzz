d1dd501acb9dd7403e5000ef99b8b506abd5cc1f
Tolvic <terence.h.mace@gmail.com>
2018-09-27 10:45:45 +0100

    added tests for negative numbers. code already passes these tests.

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,6 +3,10 @@ require './fizzbuzz'
 describe 'fizzbuzz' do
 
   describe 'numbers' do
+    it 'returns "1" when passed -1' do
+      expect(fizzbuzz(-1)).to eq -1
+    end
+
     it 'returns "1" when passed 1' do
       expect(fizzbuzz(0)).to eq 0
     end
@@ -20,6 +24,10 @@ describe 'fizzbuzz' do
   end
 
   describe 'fizz' do
+    it 'returns "fizz" when passed -3' do
+      expect(fizzbuzz(-3)).to eq 'fizz'
+    end
+
     it 'returns "fizz" when passed 3' do
       expect(fizzbuzz(3)).to eq 'fizz'
     end
@@ -42,6 +50,10 @@ describe 'fizzbuzz' do
   end
 
   describe 'buzz' do
+    it 'returns "buzz" when passed -5' do
+      expect(fizzbuzz(-5)).to eq 'buzz'
+    end
+
     it 'returns "buzz" when passed 5' do
       expect(fizzbuzz(5)).to eq 'buzz'
     end
@@ -64,6 +76,10 @@ describe 'fizzbuzz' do
   end
 
   describe 'fizzbuzz' do
+    it 'returns "fizzbuzz" when passed -15' do
+      expect(fizzbuzz(-15)).to eq 'fizzbuzz'
+    end
+
     it 'returns "fizzbuzz" when passed 15' do
       expect(fizzbuzz(15)).to eq 'fizzbuzz'
     end
